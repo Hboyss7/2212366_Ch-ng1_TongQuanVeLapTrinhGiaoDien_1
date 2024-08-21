7d5a18a84f052fde99bcea8b8670f7f38d5d6a88
Hboyss7 <150256806+Hboyss7@users.noreply.github.com>
2024-08-21 13:05:06 +0700

    Add files via upload

---
+++ 2212366_C1_TongQuanVeLapTrinhGiaoDien_1/2212366_C1_TongQuanVeLapTrinhGiaoDien_1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212366_C1_TongQuanVeLapTrinhGiaoDien_1
+{
+    public partial class panel : Form
+    {
+        public panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
